1c200c327d6937cc7dcc9a9a56468214756ec035
Deluka <216918109+Deluka93@users.noreply.github.com>
2025-07-11 14:04:35 +0200

    Function getComputerChoice complete
    Computer randomly pick up number and assign value to it

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+// Player will have choice of 3 boxes containing "rock" "paper" "scisors" buttons
+// After submiting a choice computer randomly will print one of the three following choices "rock" "paper" "scisors"
+// after responce program will display if player won or not.
+
+
+function getComputerChoice() {
+     let randomNumber = Math.floor(Math.random() * 100);
+     let gameResult;
+        if(randomNumber <= 33) {
+            gameResult = "Rock!";
+        }   else if ((randomNumber > 33) && (randomNumber <= 66)) {
+            gameResult = "Paper!";
+        }   else if (randomNumber > 66) {
+            gameResult = "Scissors!";
+        }   else {
+        }                    
+    return gameResult;     
+    }
+console.log(getComputerChoice());
